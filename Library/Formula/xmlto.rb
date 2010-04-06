9723e01f055164eab37c56a937a52f88b0037264
Michael Wild <themiwi@users.sourceforge.net>
2010-04-06 20:38:26 +0200

    Add xmlto formula
    Signed-off-by: Michael Wild <themiwi@users.sourceforge.net>

---
+++ Library/Formula/xmlto.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class Xmlto <Formula
+  url 'https://fedorahosted.org/releases/x/m/xmlto/xmlto-0.0.23.tar.bz2'
+  homepage 'http://cyberelk.net/tim/software/xmlto'
+  md5 '3001d6bb2bbc2c8f6c2301f05120f074'
+
+  depends_on 'getopt'
+  depends_on 'docbook-xml'
+
+  def install
+    ENV['XML_CATALOG_FILES'] = etc+'xml'+'catalog'
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+      "--prefix=#{prefix}", "--mandir=#{prefix}/share/man"
+    system "make install"
+  end
+end
