867d783c1a5862784c5aadf2950d3fb1965bab0d
Michael Wild <themiwi@users.sourceforge.net>
2010-04-07 09:50:38 +0200

    Install docs (w. html and pdf) in zsh formula
    Signed-off-by: Michael Wild <themiwi@users.sourceforge.net>

--- Library/Formula/zsh.rb
+++ Library/Formula/zsh.rb
@@ -1,25 +1,33 @@
 require 'formula'
 
 class Zsh <Formula
-  @url='http://downloads.sourceforge.net/project/zsh/zsh-dev/4.3.10/zsh-4.3.10.tar.gz'
-  @homepage='http://www.zsh.org/'
-  @md5='031efc8c8efb9778ffa8afbcd75f0152'
+  url 'http://downloads.sourceforge.net/project/zsh/zsh-dev/4.3.10/zsh-4.3.10.tar.gz'
+  homepage 'http://www.zsh.org/'
+  md5 '031efc8c8efb9778ffa8afbcd75f0152'
 
   def install
+    html = doc+'html'
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}",
                           "--disable-dependency-tracking",
                           # don't version stuff in Homebrew, we already do that!
-                          "--enable-fndir=#{share}/zsh/functions",
-                          "--enable-scriptdir=#{share}/zsh/scripts"
+                          "--enable-fndir=#{share+'zsh'+'functions'}",
+                          "--enable-scriptdir=#{share+'zsh'+'scripts'}",
+                          # HTML doc directory
+                          "--htmldir=#{html}"
 
     # Again, don't version installation directories
     inreplace ["Makefile", "Src/Makefile"],
       "$(libdir)/$(tzsh)/$(VERSION)", "$(libdir)"
 
-    system "make install"
+    system "make install install.html install.info pdf"
+
+    # Install docs
+    doc.install Dir['INSTALL', 'LICENCE', 'META-FAQ', 'README',
+                     'StartupFiles/*', 'ChangeLog*', 'Doc/zsh.pdf']
+    (html+'index.html').make_symlink(html+'zsh.html')
   end
-  
+
   def skip_clean? path
     true
   end
