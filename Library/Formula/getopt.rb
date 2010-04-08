a0a972f5e6f9e99a4e970999a20f1af6e64c03d5
Michael Wild <themiwi@users.sourceforge.net>
2010-04-08 07:31:19 +0200

    Formatting and add patch comment in getopt formula
    Signed-off-by: Michael Wild <themiwi@users.sourceforge.net>

--- Library/Formula/getopt.rb
+++ Library/Formula/getopt.rb
@@ -12,36 +12,21 @@ class Getopt <Formula
   end
 
   def install
-    #system "make prefix=%p WITH_GETTEXT=1 LIBCGETOPT=0 LDFLAGS=\"-L%p/lib -lintl\" localedir=%p/share/locale"
     gettext = Formula.factory('gettext')
     inreplace 'getopt.1', '@PREFIX@', prefix
     inreplace 'Makefile', /^CPPFLAGS=/, "CPPFLAGS=-I./gnu -I#{gettext.include} "
-    system "make", "prefix=#{prefix}", "WITH_GETTEXT=1", "LIBCGETOPT=0",
-      "LDFLAGS=-L#{gettext.lib} -lintl", "mandir=#{prefix}/share/man",
-      "getoptdir=#{prefix}/share/doc/getopt", "localedir=#{prefix}/share/locale",
-      "install", "install_doc"
+    system "make", "prefix=#{prefix}",
+                   "WITH_GETTEXT=1", "LIBCGETOPT=0",
+                   "LDFLAGS=-L#{gettext.lib} -lintl",
+                   "mandir=#{prefix}/share/man",
+                   "getoptdir=#{prefix}/share/doc/getopt",
+                   "localedir=#{prefix}/share/locale",
+                   "install", "install_doc"
   end
 end
-
+# Below patch is inspired by the one used in Fink. It updates the manpage
+# for the non-default examples installation directory.
 __END__
-diff --git a/Makefile b/Makefile
-index f930a28..b32137c 100644
---- a/Makefile
-+++ b/Makefile
-@@ -3,11 +3,11 @@
- DESTDIR=
- prefix=/usr/local
- bindir=$(prefix)/bin
--mandir=$(prefix)/man
-+mandir=$(prefix)/share/man
- man1dir=$(mandir)/man1
- libdir=$(prefix)/lib
- sharedir=$(prefix)/share
--getoptdir=$(libdir)/getopt
-+getoptdir=$(sharedir)/getopt
- localedir=$(sharedir)/locale
- 
- # Define this to 0 to use the getopt(3) routines in this package.
 diff --git a/getopt.1 b/getopt.1
 index 874e462..9fe39f4 100644
 --- a/getopt.1
