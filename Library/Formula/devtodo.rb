a87f5f75d6be8bafb4aa759564ddb835861ff54e
Michael Wild <themiwi@users.sourceforge.net>
2010-05-20 08:39:11 +0200

    Patch invalid regex in devtodo sources
    Signed-off-by: Michael Wild <themiwi@users.sourceforge.net>

--- Library/Formula/devtodo.rb
+++ Library/Formula/devtodo.rb
@@ -7,6 +7,10 @@ class Devtodo <Formula
 
   depends_on "readline"
 
+  def patches
+    DATA
+  end
+
   def install
     # Rename Regex.h to Regex.hh to avoid confusion with regex.h
     FileUtils.mv "util/Regex.h", "util/Regex.hh"
@@ -24,3 +28,17 @@ class Devtodo <Formula
     doc.install 'contrib'
   end
 end
+__END__
+Fix invalid regex. See http://swapoff.org/ticket/54
+
+--- a/util/XML.cc	Mon Dec 10 22:26:55 2007
++++ b/util/XML.cc	Mon Dec 10 22:27:07 2007
+@@ -49,7 +49,7 @@ void XML::init() {
+ 	// Only initialise scanners once
+ 	if (!initialised) {
+ 		// <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+-		xmlScan.addPattern(XmlDecl, "<\\?xml.*?>[[:space:]]*");
++		xmlScan.addPattern(XmlDecl, "<\\?xml.*\\?>[[:space:]]*");
+ 		xmlScan.addPattern(XmlCommentBegin, "<!--");
+ 		xmlScan.addPattern(XmlBegin, "<[a-zA-Z0-9_-]+"
+ 			"([[:space:]]+[a-zA-Z_0-9-]+=(([/a-zA-Z_0-9,.]+)|(\"[^\"]*\")|('[^']*')))"
