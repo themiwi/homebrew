73d16b555de238eadc5fa23fac19b3897d3f7cfe
Michael Wild <themiwi@users.sourceforge.net>
2010-09-01 15:49:34 +0200

    Fixed the fop.rb formula
    Signed-off-by: Michael Wild <themiwi@users.sourceforge.net>

--- Library/Formula/fop.rb
+++ Library/Formula/fop.rb
@@ -4,7 +4,6 @@ class Fop <Formula
   homepage "http://xmlgraphics.apache.org/fop/index.html"
   url "http://mirrors.ibiblio.org/pub/mirrors/apache/xmlgraphics/fop/binaries/fop-1.0-bin.tar.gz"
   md5 "3186f93a314bdcb710bd7cb02d80404c"
-  aka 'apache-fop'
 
   def shim_script target
     <<-EOS.undent
